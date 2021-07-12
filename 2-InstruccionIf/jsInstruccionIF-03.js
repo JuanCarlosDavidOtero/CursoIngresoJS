cb3acac6a1e7b77b984f369f8ced249ce9884ed1
juancarlosdavidotero <juancarlosdavidotero@gmail.com>
2021-07-12 19:42:53 -0300

    cambios

--- 2-InstruccionIf/jsInstruccionIF-03.js
+++ 2-InstruccionIf/jsInstruccionIF-03.js
@@ -1,6 +1,17 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	var edad;
+	edad = txtIdEdad.value;
+	edad = parseInt(edad);
 
-}//FIN DE LA FUNCIÓN
+	if(edad <= 18)
+	{
+		alert("Sos menor de edad");
+	}
+	else
+	{
+		alert("Sos mayor de edad")
+	}
+	
+
+}
